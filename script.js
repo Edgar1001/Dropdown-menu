518b28c27d1589e02df6ba54cb154955b8a6c9f1
Edgar1001 <43092559+Edgar1001@users.noreply.github.com>
2023-01-31 20:49:37 +0100

    Login

--- script.js
+++ script.js
@@ -2,7 +2,41 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-   hamburger.classList.toggle("active");
-   navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-})
+window.onload = function() {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const keyword = document.querySelector("#keyword").value;
+    const location = document.querySelector("#location").value;
+    fetch("jobs-data.json")
+      .then(response => response.json())
+      .then(data => {
+        const filteredJobs = data.jobs.filter(job => {
+          return job.title.includes(keyword) && job.location.includes(location);
+        });
+        const jobsList = document.querySelector("#jobsList");
+        jobsList.innerHTML = "";
+        filteredJobs.forEach(job => {
+          const newListItem = document.createElement("li");
+          newListItem.innerHTML = `
+          <h3>${job.title}<h3> 
+          <p>${job.company}</p>
+          <p>${job.location}</p>
+          <p>${job.experience}</p>
+          <a href="${job.apply_link}">Apply now</a>
+          `
+          jobsList.appendChild(newListItem);
+        });
+      });
+  });
+}
+  
+
+
+
+
